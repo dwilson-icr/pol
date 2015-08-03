3589f67aea08cdcda6cc22e3f0871b32434c19de
Carly Meyer <carlz@Carlys-MacBook-Pro.local>
2015-08-03 10:36:26 -0600

    Edited manipulate data

--- sandbox/manipulatedata.py
+++ sandbox/manipulatedata.py
@@ -1,33 +1,59 @@
 # a is a list of location dictionaries
-a = [{
-    "timestampMs" :
-    "latitudeE7" :
-    "longitudeE7" :
-    "accuracy" :
+a = [
+{
+    "timestampMs" : "1399222947211",
+    "latitudeE7" : 396124028,
+    "longitudeE7" : -1047693543,
+    "accuracy" : 70
+  }, {
+    "timestampMs" : "1399222879731",
+    "latitudeE7" : 396124046,
+    "longitudeE7" : -1047693466,
+    "accuracy" : 69
+  }, {
+    "timestampMs" : "1399222811733",
+    "latitudeE7" : 396124169,
+    "longitudeE7" : -1047693292,
+    "accuracy" : 69
   }
-  ]
-import time
-from datetime import date
+]
+
+from datetime import datetime
+import geocoder
 
 for dicts in a:
     for attributes in dicts:
+        # changes timestampMs into a usable format
         time_params = float(dicts.get("timestampMs"))
-        # w, m, d, Y, h convert timestampMs into day of week, month, day, year
-        # and hour of day, respectively
-        w = time.strftime("%w", time.localtime(time_params / 1000.0))
-        m = time.strftime("%m", time.localtime(time_params / 1000.0))
-        d = time.strftime("%d", time.localtime(time_params / 1000.0))
-        Y = time.strftime("%Y", time.localtime(time_params / 1000.0))
-        h = time.strftime("%H", time.localtime(time_params / 1000.0))
+        time_params *= .001
+        # datetime.fromtimestamp gives date and time data from a timestamp
+        dtime = datetime.fromtimestamp(time_params)
+        w = datetime.weekday(dtime)
+        m = dtime.month
+        d = dtime.day
+        Y = dtime.year
+        h = dtime.hour
         # creates a pair of lat-long coordinates
         latlong = (float(dicts["latitudeE7"])*1e-7,\
                     float(dicts["longitudeE7"])*1e-7)
+        # g.address gives address corresponding to lat-long pair
+        g = geocoder.google(latlong, method='reverse')
+        g = g.address
     # update dictionaries with relevant information and deletes irrelavant info
     dicts.update({"day of week": w, "month": m, "day": d, "year": Y, "hour": h,\
-                "lat-long": latlong})
+                "address": g})
+    # deletes unused attributes in dicts to expedite libpgm process
     del dicts["timestampMs"]
     del dicts["latitudeE7"]
     del dicts["longitudeE7"]
+    if "activitys" in dicts:
+        del dicts["activitys"]
+    if "velocity" in dicts:
+        del dicts["velocity"]
+    if "heading" in dicts:
+        del dicts["heading"]
+    if "altitude" in dicts:
+        del dicts["altitude"]
 
 import json
 with open("manipulatedata.json","w") as fp:
